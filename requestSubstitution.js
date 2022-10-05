26ae6db00592828bb516c9b140217d56df4bf547
akcenurt <98740756+akcenurt@users.noreply.github.com>
2022-10-05 09:26:03 +0200

    Update requestSubstitution.js

--- requestSubstitution.js
+++ requestSubstitution.js
@@ -31,18 +31,6 @@ beforeEach( () => {
 
 });
 
-it('zoznam dvoch boardov z fixture', () => {
-
-  // použi súbor twoBoards.json z fixtures foldra. do tohto it() bloku nemusíš nič písať. stačí, že zmeníš .route() príkaz v beforeEach bloku
-
-})
-
-it('zoznam boardov s hviezdickou z fixture', () => {
-
-  // ak sa Ti podarilo splniť predošlú úlohu, stačí si vyfiltrovať tento test a zmeniť dáta v súbore twoBoards.json po uložení testu a súboru twoBoards.json by si mal/a vidieť správny výsledok
-
-})
-
 it.only('chyba pri vytvoreni tasku', () => {
 
   cy.get(".board_item").eq(0)
@@ -56,7 +44,5 @@ it.only('chyba pri vytvoreni tasku', () => {
 
   cy.get("#errorMessage")
   .should("be.visible")
-
-  // pridaj si ďalší .route() príkaz, ktorým si odchytíš api volanie, ktoré sa robí pri vytvorení tasku. nahraď odpoveď tak, aby zo servra prišiel chybový status kód. dôležitá poznámka: pri nahradzovaní odpovede je parameter „response“ povinný
 
 })
